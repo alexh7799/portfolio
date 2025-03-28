42da829a32feee08e41bcef9b876303ce95af1a0
Alexander Hörst <hoerst.alexander@gmail.com>
2025-03-28 19:30:27 +0100

    add validate and send function

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
@@ -14,7 +14,6 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),importProvidersFrom(
-    HttpClientModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -23,5 +22,5 @@ export const appConfig: ApplicationConfig = {
       },
       defaultLanguage: 'de'
     })
-  ), provideFirebaseApp(() => initializeApp({"projectId":"portfolio-14d2c","appId":"1:966344824986:web:f4bb2b5ab4cef7970fa723","storageBucket":"portfolio-14d2c.firebasestorage.app","apiKey":"AIzaSyD0kDUm2C3D3ox3sYmfigDsvf7fITIvAeE","authDomain":"portfolio-14d2c.firebaseapp.com","messagingSenderId":"966344824986"})), provideFirestore(() => getFirestore())]
+  ),provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"portfolio-14d2c","appId":"1:966344824986:web:f4bb2b5ab4cef7970fa723","storageBucket":"portfolio-14d2c.firebasestorage.app","apiKey":"AIzaSyD0kDUm2C3D3ox3sYmfigDsvf7fITIvAeE","authDomain":"portfolio-14d2c.firebaseapp.com","messagingSenderId":"966344824986"})), provideFirestore(() => getFirestore())]
 };
--- src/app/landing-page/contact/contact.component.ts
+++ src/app/landing-page/contact/contact.component.ts
@@ -1,92 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonsComponent } from "../../shared/buttons/buttons.component";
 import { ArrowComponent } from "../../shared/arrow/arrow.component";
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ButtonsComponent, ArrowComponent, CommonModule, TranslateModule, FormsModule],
+  imports: [ArrowComponent, CommonModule, TranslateModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 
 export class ContactComponent {
+  http = inject(HttpClient);
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://localhost/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  readonly MIN_LENGTH = 4;
+  readonly EMAIL_PATTERN = "[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}";
+
   formData = {
     name: '',
     email: '',
     message: '',
     policy: false
   };
-  
+
   formErrors = {
     name: false,
     email: false,
     message: false,
     policy: false
   };
-
-  validateForm() {
-    // Reset errors
-    this.formErrors = {
-      name: false,
-      email: false,
-      message: false,
-      policy: false
-    };
-
-    let hasError = false;
-
-    if (!this.formData.name.trim()) {
-      this.formErrors.name = true;
-      hasError = true;
-    }
-    if (!this.formData.email.trim()) {
-      this.formErrors.email = true;
-      hasError = true;
-    }
-    if (!this.formData.message.trim()) {
-      this.formErrors.message = true;
-      hasError = true;
-    }
-    if (!this.formData.policy) {
-      this.checkboxState.hasError = true;
-      this.formErrors.policy = true;
-      hasError = true;
-  }
-
-    if (!hasError) {
-      // Hier kommt die Sende-Logik
-      console.log('Form is valid', this.formData);
-    }
-  }
-
-  checkboxState = {
-    isChecked: false,
-    isHovered: false,
-    hasError: false
-  };
-
-  getCheckboxImage(): string {
-    if (this.checkboxState.hasError) {
-      return 'assets/img/checkbox-error.svg';
-    }
-    if (this.checkboxState.isChecked) {
-      return 'assets/img/checkbox-checked.svg';
-    }
-    if (this.checkboxState.isHovered) {
-      return 'assets/img/checkbox-hover.svg';
-    }
-    return 'assets/img/checkbox-default.svg';
-  }
-
-  toggleCheckbox() {
-    this.checkboxState.isChecked = !this.checkboxState.isChecked;
-    this.checkboxState.hasError = false;
-    this.formData.policy = this.checkboxState.isChecked;
-  }
 
   contactIcons = {
     email: {
@@ -111,6 +70,84 @@ export class ContactComponent {
     }
   };
 
+  checkboxState = {
+    isChecked: false,
+    isHovered: false,
+    hasError: false
+  };
+
+  validateForm() {
+    this.formErrors = {
+      name: false,
+      email: false,
+      message: false,
+      policy: false
+    };
+
+    let hasError = false;
+
+    if (this.formData.name.trim().length < this.MIN_LENGTH) {
+      this.formErrors.name = true;
+      hasError = true;
+    }
+    if (!this.formData.email.match(this.EMAIL_PATTERN)) {
+      this.formErrors.email = true;
+      hasError = true;
+    }
+    if (this.formData.message.trim().length < this.MIN_LENGTH) {
+      this.formErrors.message = true;
+      hasError = true;
+    }
+
+    if (!this.formData.policy) {
+      this.checkboxState.hasError = true;
+      this.formErrors.policy = true;
+      hasError = true;
+    }
+
+    return !hasError;
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if(this.validateForm()) {
+      if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+        this.http.post(this.post.endPoint, this.post.body(this.formData))
+          .subscribe({
+            next: (response) => {
+
+              ngForm.resetForm();
+            },
+            error: (error) => {
+              console.error(error);
+            },
+            complete: () => console.info('send post complete'),
+          });
+      } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+        ngForm.resetForm();
+      }
+    }
+  }
+
+  getCheckboxImage(): string {
+    if (this.checkboxState.hasError) {
+      return 'assets/img/checkbox-error.svg';
+    }
+    if (this.checkboxState.isChecked) {
+      return 'assets/img/checkbox-checked.svg';
+    }
+    if (this.checkboxState.isHovered) {
+      return 'assets/img/checkbox-hover.svg';
+    }
+    return 'assets/img/checkbox-default.svg';
+  }
+
+  toggleCheckbox() {
+    this.checkboxState.isChecked = !this.checkboxState.isChecked;
+    this.checkboxState.hasError = false;
+    this.formData.policy = this.checkboxState.isChecked;
+  }
+
   getContactImage(type: 'email' | 'phone'): string {
     if (this.contactIcons[type].isActive) {
       return this.contactIcons[type].active;
@@ -119,5 +156,19 @@ export class ContactComponent {
       return this.contactIcons[type].hover;
     }
     return this.contactIcons[type].default;
+  }
+
+  getValidationIcon(field: 'name' | 'email' | 'message'): string {
+    if (this.formData[field] && !this.formErrors[field]) {
+      return 'assets/img/input-check.svg';
+    }
+    if (this.formErrors[field]) {
+      return 'assets/img/input-error.svg';
+    }
+    return '';
+  }
+
+  shouldShowIcon(field: 'name' | 'email' | 'message'): boolean {
+    return this.formData[field].length > 0 || this.formErrors[field];
   }
 }
